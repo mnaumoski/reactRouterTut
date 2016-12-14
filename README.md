d86f0ee93f3dd44dd608475be6166708feba8553
mnaumoski <naumoski.marija@gmail.com>
2016-12-14 14:00:48 -0500

    production server is up

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # reactRouterTut
 ## Practice react-router-tutorial
 
+These lessons are beneficial in learining React's routing process.
+
--- index.js
+++ index.js
@@ -4,15 +4,26 @@ import App from './modules/App'
 // import modules About and Repos
 import About from './modules/About'
 import Repos from './modules/Repos'
+// import Repo
+import Repo from './modules/Repo'
+// importing router, route and hash
+import {Router, Route, browserHistory, IndexRoute} from 'react-router'
 
-render(<App/>, document.getElementById('app'))
 
-import {Router, Route, hashHistory} from 'react-router'
+// and the Home component
+import Home from './modules/Home'
+
+// render App component in the div with id of app
+// render(<App/>, document.getElementById('app'))
+// render the paths
 render((
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path= "/" component={App} >
-// add the routes paths
-        <Route path="/repos" component={Repos}/>
+        <IndexRoute component={Home}/>
+        <Route path="/repos" component={Repos}>
+            <Route path="/repos/:userName/:repoName" component={Repo}/>
+        </Route>
         <Route path="/about" component={About}/>
     </Route>
-  </Router>), document.getElementById('app'))
+  </Router>
+  ), document.getElementById('app'))
